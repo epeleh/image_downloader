8e090f28d8f18ad0397440737ea7ec3a5a654556
Evgeny Peleh <pelehev@gmail.com>
2022-11-22 14:38:07 +0300

    Update rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,22 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before do |example|
+    next if config.files_to_run.one?
+
+    path = example.metadata.dig(:example_group, :file_path)
+    current_path = config.instance_variable_get(:@current_file_path)
+
+    if current_path.nil? || path != current_path
+      config.instance_variable_set(:@current_file_path, path)
+      puts unless current_path.nil?
+      print "#{path} "
+    end
   end
 end
